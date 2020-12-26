7913af1c2f505abdd8a44712e28ad186c977ed2c
zhaxu <zhaxu@microstrategy.com>
2020-12-26 21:49:30 +0800

    Initial Commit

---
+++ XTrackPad/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  XTrackPad
+//
+//  Created by xuzhaocheng on 2020/12/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ XTrackPad/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  XTrackPad
+//
+//  Created by xuzhaocheng on 2020/12/26.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
