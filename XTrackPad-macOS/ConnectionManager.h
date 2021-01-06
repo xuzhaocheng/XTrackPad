0e9a9573e04e3e816baae8dedcb1b11ae8a366d8
zhaxu <zhaxu@microstrategy.com>
2021-01-06 21:51:59 +0800

    Refine macOS App UI

---
+++ XTrackPad-macOS/ConnectionManager.h
@@ -0,0 +1,16 @@
+//
+//  ConnectionManager.h
+//  XTrackPad-macOS
+//
+//  Created by xuzhaocheng on 2021/1/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ConnectionManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XTrackPad-macOS/ConnectionManager.m
@@ -0,0 +1,12 @@
+//
+//  ConnectionManager.m
+//  XTrackPad-macOS
+//
+//  Created by xuzhaocheng on 2021/1/5.
+//
+
+#import "ConnectionManager.h"
+
+@implementation ConnectionManager
+
+@end
